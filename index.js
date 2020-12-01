1590de867ba4776123bba03e4cc6f0c1a5d05a7e
Fred Peixoto <fred.peixoto@softwillians.com.br>
2020-12-01 18:01:01 -0300

    split method to remove from packet

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ for (let index = 0; index < linePayment.length; index++) {
     }
 
     if (!thereIsChange) {
-        console.log("Pocket is empty for it payment!");
+        console.log(pocket, "Pocket is empty for it payment!");
         break;
     }
 
@@ -45,25 +45,29 @@ function thereIsBill(changeNecessary) {
     let wasChangeOk = false;
     console.log('changeNecessary', changeNecessary);
     console.log('pocket now', pocket);
-    if (changeNecessary >= 50 && pocket.some(bill => bill == 50)) {
-        let indexRemove = pocket.findIndex(bill => bill == 50);
-        changeNecessary -= 50;
-        pocket.splice(indexRemove, 1);
-    }
-    else if (changeNecessary >= 25 && pocket.some(bill => bill == 25)) {
-        let indexRemove = pocket.findIndex(bill => bill == 25);
-        changeNecessary -= 25;
-        pocket.splice(indexRemove, 1);
-    }
+    if (changeNecessary >= 50)
+        changeNecessary = removeFromPocket(changeNecessary, 50);
+    else if (changeNecessary == 25)
+        changeNecessary = removeFromPocket(changeNecessary, 25);
 
     if (changeNecessary === 0) {
         wasChangeOk = true;
-
     }
-    else
+    else if (pocket.length === 0)
+        wasChangeOk = false;
+    else{
         thereIsBill(changeNecessary);
+        break;
+    }
 
     return wasChangeOk;
+}
 
-
+function removeFromPocket(changeNecessary, bill) {
+    if (changeNecessary >= bill && pocket.some(_bill => _bill == bill)) {
+        let indexRemove = pocket.findIndex(_bill => _bill == bill);
+        changeNecessary -= bill;
+        pocket.splice(indexRemove, 1);
+    }
+    return changeNecessary;
 }
